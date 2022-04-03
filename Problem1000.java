71295bbb1fa9170556d66b33ec38bd86ebf1da42
이예솔 <86811852+0103lys@users.noreply.github.com>
2022-04-03 21:07:48 +0900

    1000
    1000

---
+++ Problem1000.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Problem1000 {
+    public int solution(int a, int b){
+        int answer = 0;
+        answer = a + b;
+        return answer;
+
+    }
+    public static void main(String[] args) {
+        Problem1000 problem1000 = new Problem1000();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(problem1000.solution(a, b));
+    }
+}
