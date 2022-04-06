f0f5e2979f80dcc1fe1abf4af66351615801aec7
이예솔 <86811852+0103lys@users.noreply.github.com>
2022-04-07 03:22:41 +0900

    Create Problem2739.java

---
+++ Problem2739.java
@@ -0,0 +1,14 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Problem2739 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dan = sc.nextInt();
+
+        for(int i=1; i<10; i++){
+            System.out.println(dan + " * "+ i + " = "+(dan*i));
+        }
+    }
+}
