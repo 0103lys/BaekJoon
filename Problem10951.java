e0b957104212b6e7cbeffe455e5004f644c50e59
이예솔 <86811852+0103lys@users.noreply.github.com>
2022-04-09 16:11:34 +0900

    Create Problem10951.java

---
+++ Problem10951.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Problem10951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+
+    }
+}
